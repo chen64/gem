e648ca14061b1de4ec59607e4fb6ab18073373d9
Jayson <jaysonchen64@gmail.com>
2018-03-12 12:51:16 -0500

    some fixand bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,6 @@ $(document).ready(function(){
     var wins=0;
     var lose=0;
 
-    function reset(){
-
-    }
-
     $("#wins").text(wins);
     $("#lose").text(lose);
     $("#points").text(total);
@@ -28,60 +24,80 @@ $(document).ready(function(){
         {
             win++;
             $("#wins").text(wins);
+            reset();
             
         }
         else if(total > number)
         {
             lose++;
-            $("#wins").text(lose);
+            $("#lose").text(lose);
+            reset();
         }
     })
 
-    $("#add1").on("click", function(){
-        total= total+num1;
+    $("#add2").on("click", function(){
+        total= total+num2;
         $("#points").text(total);
         if(total == number)
         {
             win++;
             $("#wins").text(wins);
+            reset();
+           
             
         }
         else if(total > number)
         {
             lose++;
-            $("#wins").text(lose);
+            $("#lose").text(lose);
+            reset();
         }
     })
 
-    $("#add1").on("click", function(){
-        total= total+num1;
+    $("#add3").on("click", function(){
+        total= total+num3;
         $("#points").text(total);
         if(total == number)
         {
             win++;
             $("#wins").text(wins);
+            reset();
+        
             
         }
         else if(total > number)
         {
             lose++;
-            $("#wins").text(lose);
+            $("#lose").text(lose);
+            reset();
         }
     })
 
-    $("#add1").on("click", function(){
-        total= total+num1;
+    $("#add4").on("click", function(){
+        total= total+num4;
         $("#points").text(total);
         if(total == number)
         {
             win++;
             $("#wins").text(wins);
-            
+            reset();
         }
         else if(total > number)
         {
             lose++;
-            $("#wins").text(lose);
+            $("#lose").text(lose);
+            reset();
         }
     })
+
+    function reset(){
+        number=Math.floor(Math.random()* 101 +19);
+        $("#answer").text(number);
+        num1=Math.floor(Math.random()* 11 +1);
+        num2=Math.floor(Math.random()* 11 +1);
+        num3=Math.floor(Math.random()* 11 +1);
+        num4=Math.floor(Math.random()* 11 +1);
+        total=0;
+        $("#points").text(total);
+    }
 });
